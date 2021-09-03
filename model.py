a3562948a73bef8fdf00dc39e7ab385474599707
Inenarratus <88681966+Inenarratus@users.noreply.github.com>
2021-09-03 16:09:05 +0300

    Update model.py

--- model.py
+++ model.py
@@ -4,8 +4,8 @@ from networks.models import build_model
 class Args:
     def __init__(self):
         self.encoder = 'resnet50_GN_WS'
-        self.decoder = 'fba_decoder'
-        self.weights = 'FBA.pth'
+        self.decoder = 'bgr_decoder'
+        self.weights = 'BGR.pth'
     
 args = Args()
 Model = build_model(args)
